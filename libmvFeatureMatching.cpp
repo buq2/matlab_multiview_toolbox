3f3b949e172205d51e1a5be8084399afa0850a4d
buq2 <buq2@2628a70f-36ec-412d-a57e-0a82f932ded2>
2011-07-23 17:01:18 +0000

    Toimiva pistepariehdokkaiden etsintä perustuen etäisyyteen
    git-svn-id: https://buq22.ath.cx:1941/svn/matlab/trunk/multiview_toolbox@2843 2628a70f-36ec-412d-a57e-0a82f932ded2

--- libmvFeatureMatching.cpp
+++ libmvFeatureMatching.cpp
@@ -25,7 +25,7 @@ void mexFunction(int        nlhs,        /*(NumLeftHandSide) Number of arguments
     //
     // Some MATLAB error checking
     //
-    
+
     if (nrhs != 2) {
         //Not enough inputs
         usage();
@@ -48,13 +48,7 @@ void mexFunction(int        nlhs,        /*(NumLeftHandSide) Number of arguments
         mexErrMsgTxt("Input data must be 3xn 2D matrix\n");
     }
 
-/*
-    if (dimsData1[1] != dimsData2[1]) {
-        usage();
-        mexErrMsgTxt("Input matrices must be same size\n");
-    }
-*/
-    if (nlhs != 1) {
+    if (nlhs != 2) {
         usage();
         mexErrMsgTxt("Wrong number of outputs\n");
     }
@@ -66,40 +60,76 @@ void mexFunction(int        nlhs,        /*(NumLeftHandSide) Number of arguments
     unsigned char *data1 = (unsigned char*)mxGetData(prhs[0]);
     unsigned char *data2 = (unsigned char*)mxGetData(prhs[1]);
     int mRow = dimsData1[0]; //In MATLAB indexing
-    int mCol = dimsData1[1];
+
+    int mCol1 = dimsData1[1]; //Number of points in set 1
+    int mCol2 = dimsData2[1]; //Number of points in set 2
 
     //
     // Create libmv data
     //
 
-/*
-    libmv::FeatureSet features1;
-    libmv::FeatureSet features2;
-    libmv::FeatureSet::Iterator<libmv::PointFeature> it1 = features1.Insert<libmv::PointFeature>(libmv::PointFeature(data1[0],data1[1]));
-    libmv::FeatureSet::Iterator<libmv::PointFeature> it2 = features1.Insert<libmv::PointFeature>(libmv::PointFeature(data2[0],data2[1]));
-    for (unsigned int ii = 1; ii < mCol; ++ii) {
-        it1 = features1.Insert<libmv::PointFeature>(libmv::PointFeature(data1[ii*3 + 0],data1[ii*3 + 1]));
-        it2 = features1.Insert<libmv::PointFeature>(libmv::PointFeature(data2[ii*3 + 0],data2[ii*3 + 1]));
-    }
-*/
+    //Points are stored in two feature sets. Feature sets
+    //can be used to store points, feature descriptors etc.
     FeatureSet features1;
     FeatureSet features2;
 
+    //Convert both homogenous point sets to FeatureSets
+    for (unsigned int ii = 0; ii < mCol1; ++ii) {
+        //Normalize (wnorm)
+        double w = data1[ii*3+2];
 
-    for (unsigned int ii = 0; ii < mCol; ++ii) {
-        libmv::PointFeature p1(data1[ii*3 + 0],data1[ii*3 + 1]);
+        //First create sinle point (this step could be replaced by just creating .coords)
+        libmv::PointFeature p1(data1[ii*3 + 0]/w,data1[ii*3 + 1]/w);
+
+        //As FeatureSets camparisons (matching) is done using feature descriptors
+        //we convert the point coordinates (x,y) to descriptor
         KeypointFeature kp1;
         kp1.descriptor = libmv::descriptor::VecfDescriptor(p1.coords);
-        features1.features.push_back(kp1);
 
-        libmv::PointFeature p2(data2[ii*3 + 0],data2[ii*3 + 1]);
+        //Now we add this descriptor of single point to feature set
+        features1.features.push_back(kp1);
+    }
+
+    //Exactly same thing for second points
+    for (unsigned int ii = 0; ii < mCol2; ++ii) {
+        double w = data2[ii*3+2];
+        libmv::PointFeature p2(data2[ii*3 + 0]/w,data2[ii*3 + 1]/w);
         KeypointFeature kp2;
         kp2.descriptor = libmv::descriptor::VecfDescriptor(p2.coords);
         features2.features.push_back(kp2);
     }
-    
+
+    //Result will be in Matches
     libmv::Matches matches;
+
+    //See documentation of FindSymmetricCandidateMatches_FLANN
+    //Basically for each point (p1) in set 1, finds closest point from set 2 (p2) and checks if
+    //p1 is closest point from set 1 for p2, if true, add to matches
     FindSymmetricCandidateMatches_FLANN(features1,features2,&matches);
 
-    mexPrintf("%d - %d\n",matches.NumTracks(),matches.NumImages());
+    //matches.NumTracks() will be number of point matches after removing (part of the) outliers
+    //Unfortunately, at the time of writing of this function. Original index numbers of the features can not be determined
+    //(would require addition of 'tag' or 'idx' to KeypointFeature
+    mwSize dims_out1[3] = {3, matches.NumTracks()};
+    plhs[0] = mxCreateNumericArray(2, dims_out1, mxDOUBLE_CLASS, mxREAL);
+    plhs[1] = mxCreateNumericArray(2, dims_out1, mxDOUBLE_CLASS, mxREAL);
+
+    double *outdata1 = (double*)mxGetData(plhs[0]);
+    double *outdata2 = (double*)mxGetData(plhs[1]);
+    for (unsigned int ii = 0; ii < dims_out1[1]; ++ii) {
+        const libmv::Feature *f1 = matches.Get(0,ii); //From img id 0
+        const libmv::Feature *f2 = matches.Get(1,ii); //From img id 1
+
+        KeypointFeature *kp1 = (KeypointFeature*)f1;
+        KeypointFeature *kp2 = (KeypointFeature*)f2;
+
+        outdata1[ii*3 + 0] = kp1->descriptor.coords(0);
+        outdata1[ii*3 + 1] = kp1->descriptor.coords(1);
+        outdata1[ii*3 + 2] = 1.0; //Normalized
+
+        outdata2[ii*3 + 0] = kp2->descriptor.coords(0);
+        outdata2[ii*3 + 1] = kp2->descriptor.coords(1);
+        outdata2[ii*3 + 2] = 1.0; //Normalized
+    }
+
 }
