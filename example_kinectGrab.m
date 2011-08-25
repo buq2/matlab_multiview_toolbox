0f09cb3e475dc0506e877e51b1e7c4d92c40ebf4
buq2 <buq2@2628a70f-36ec-412d-a57e-0a82f932ded2>
2011-08-25 05:01:44 +0000

    Ilmeisesti täysin toimiva värin interpolointi
    git-svn-id: https://buq22.ath.cx:1941/svn/matlab/trunk/multiview_toolbox@2918 2628a70f-36ec-412d-a57e-0a82f932ded2

--- example_kinectGrab.m
+++ example_kinectGrab.m
@@ -1,7 +1,11 @@
 K_rgb = reshape([5.2264754445438268e+02, 0., 3.1898233378335067e+02, 0.,5.2281923529567291e+02, 2.4004311532231731e+02, 0., 0., 1.],[3 3])';
 distortion_rgb = [2.6500366608125003e-01, -9.3179278499705598e-01,-9.5362199394449711e-04, 1.0121010318791741e-03,1.1227544625552663e+00];
+radial_rgb = distortion_rgb([1 2 5]);
+tangential_rgb = distortion_rgb([3 4]);
 K_depth = reshape([5.8680160380538359e+02, 0., 3.0852862818515104e+02, 0., 5.8765587998528531e+02, 2.3483705083973686e+02, 0., 0., 1.],[3 3])';
 distortion_depth = [-1.3543579586687302e-01, 6.4945176104838909e-01, -1.9691140916813683e-03, -2.6837429982855118e-03, -1.0090299717545017e+00];
+radial_depth = distortion_depth([1 2 5]);
+tangential_depth = distortion_depth([3 4]);
 R = reshape([9.9994855279432382e-01, -9.2814542247140770e-03, 4.0922331326492334e-03, 9.2385810989796761e-03, 9.9990350964648000e-01, 1.0374006744223806e-02, -4.1881241403509571e-03, -1.0335666602893243e-02,9.9993781487253441e-01],[3 3])';
 T = [2.4158781090188233e-02, -2.5259041391054410e-03,2.5188423349242228e-03]';
 depth_base_and_offset = [7.84662664e-02, 1.07061072e+03];
@@ -115,3 +119,73 @@ for ii = 1:5:numFrames
     zlabel('z')
     drawnow
 end
+%% Remove distortions
+ii = 1;
+jj = 1;
+img1 = rgb(:,:,:,jj);
+img2 = depth(:,:,jj);
+
+invalidDepth = img2 == 2^11-1;
+
+rgb_ud = undistortImage(img1,K_rgb,radial_rgb,tangential_rgb);
+depth_ud = undistortImage(img2,K_depth,radial_depth,tangential_depth);
+invalid_ud = undistortImage(single(invalidDepth),K_depth,radial_depth,tangential_depth);
+
+invalidDepth = invalid_ud ~= 0;
+realDepth = depth_base_and_offset(1)*mean([K_depth(1,1),K_depth(2,2)]) ./ (1/8 * (depth_base_and_offset(2) - depth_ud));
+%realDepth = 1.0 ./ (depth_ud * -0.0030711016 + 3.3309495161);
+
+w = 640;
+h = 480;
+[pixX pixY] = meshgrid(0:w-1,0:h-1);
+X = bsxfun(@times,(pixX - K_depth(1,3))/K_depth(1,1), realDepth);
+Y = bsxfun(@times,(pixY - K_depth(2,3))/K_depth(2,2), realDepth);
+Z = realDepth;
+
+%colorPosX = ((X+T(1)) * K_rgb(1,1) ./ (Z+T(3))) + K_rgb(1,3);
+%colorPosY = ((Y+T(2)) * K_rgb(2,2) ./ (Z+T(3))) + K_rgb(2,3);
+
+colorPosX = ((X-T(1)) * K_rgb(1,1) ./ (Z+T(3))) + K_rgb(1,3);
+colorPosY = ((Y-T(2)) * K_rgb(2,2) ./ (Z+T(3))) + K_rgb(2,3);
+
+colorPosInvalid = colorPosX<1 | colorPosY<1 | colorPosX>w | colorPosY>h | invalidDepth;
+colorPosX(colorPosInvalid) = 0;
+colorPosY(colorPosInvalid) = 0;
+
+X(colorPosInvalid) = NaN;
+Y(colorPosInvalid) = NaN;
+Z(colorPosInvalid) = NaN;
+
+R = interp2(pixX,pixY, single(rgb_ud(:,:,1,ii)), colorPosX(:,:,ii), colorPosY(:,:,ii));
+G = interp2(pixX,pixY, single(rgb_ud(:,:,2,ii)), colorPosX(:,:,ii), colorPosY(:,:,ii));
+B = interp2(pixX,pixY, single(rgb_ud(:,:,3,ii)), colorPosX(:,:,ii), colorPosY(:,:,ii));
+
+R = uint8(R);
+G = uint8(G);
+B = uint8(B);
+
+%Calculate straightening matrix for 3D
+up = upVec(:,ii);
+right = rightVec(:,ii);
+forward = forVec(:,ii);
+
+Rst = [forward up right   zeros(3,1);zeros(1,3) 1]';
+
+x = X(:,:,ii);
+y = Y(:,:,ii);
+z = Z(:,:,ii);
+
+dat = [x(:) y(:) z(:) ones(numel(x),1)]';
+dat = wnorm(inv(Rst)*dat);
+x(:) = -dat(1,:);
+y(:) = dat(3,:);
+z(:) = dat(2,:);
+
+
+surf(x,y,z,cat(3,R,G,B),'edgecolor','none','FaceColor','texturemap');
+%set(gca,'zDir','reverse')
+axis equal
+xlabel('x')
+ylabel('y')
+zlabel('z')
+drawnow
