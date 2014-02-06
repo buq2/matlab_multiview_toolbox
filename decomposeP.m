36b392fe93e3836e76be9760c61f012cd277d8ae
Matti Jukola <matti.jukola@pixact.fi>
2014-02-06 15:20:35 +0200

    Patches from SVN

--- decomposeP.m
+++ decomposeP.m
@@ -10,6 +10,7 @@ function [K R C] = decomposeP(P)
 %HZ2 6.2.4 p.163
 %
 %Matti Jukola 2010
+
 [K R] = rq(P(1:3,1:3));
 %Multiple ways to solve camera location
 %C2 = -P(1:3,1:3)\P(:,4)
@@ -20,4 +21,24 @@ C = V(:,end);
 %     det([P(:,1) P(:,2) P(:,4)]); ...
 %    -det([P(:,1) P(:,2) P(:,3)])]
 C = wnorm(C);
-K = K./K(end); %Added 2010.07.26
+
+% Make sure that diagonal values of the K
+% are positive
+T = -R*C(1:3);
+sigs = sign(diag(K));
+for ii = 1:numel(sigs)
+    if sigs(ii) < 0
+        tmp = eye(3);
+        tmp(ii,ii) = -1;
+        K = K*tmp;
+        R = tmp\R;
+        T = tmp\T;
+    end
+end
+% Recalculate C
+C = [-inv(R)*T;1];
+
+% We could make sure that last element of K is 1
+% which would make certain calculations easier, but
+% this would cause K*[R -RC] not to be exactly same as P
+%K = K./norm(K(end));
--- plotCamera.m
+++ plotCamera.m
@@ -11,6 +11,7 @@ function h_out = plotCamera(P,siz,color,text_plot)
 %
 %Matti Jukola 2010
 %Updated 2011.10.11
+%Updated 2013.09.15
 
 if nargin < 2 || isempty(siz)
     siz = 1;
@@ -36,17 +37,7 @@ sizeimg(sizeimg==0) = 1; %Force to have at least 1 pixel.
 x = [0 0 1 1; %Four corners of image plane
      0 1 1 0;
      1 1 1 1];
-x(1:2,:) = bsxfun(@times, x(1:2,:), sizeimg);
-
-n = wnorm(pinv(P)*x); %Vectors pointing away from camera
-n(1:3,:) = bsxfun(@minus,n(1:3,:),C(1:3,:));
-%Normalize to unit lenght
-nnorm = n;
-nnorm(1:3,:) = bsxfun(@rdivide, n(1:3,:), sqrt(sum(n.^2)));
-nnorm(1:3,:) = nnorm(1:3,:)*siz;
-
-X = nnorm;
-X(1:3,:) = bsxfun(@plus,X(1:3,:),C(1:3)); %Add camera location to corner points
+X = ProjectToImagePlane(P,sizeimg,siz,C,x);
 
 hold on
 %Plot frame
@@ -66,3 +57,42 @@ end
 if nargin > 3
     text(C(1),C(2),C(3),text_plot)
 end
+
+
+%Plot small arrows point directions of the x- and y-axis of the image plane
+x_arrow_x =  [0.2 0 0  0.2
+              0 0.05 -0.05 0
+              1 1   1   1];
+x_arrow_y =  x_arrow_x([2 1 3], :);
+
+X_arrow_x = ProjectToImagePlane(P,sizeimg, siz, C, x_arrow_x);
+X_arrow_y = ProjectToImagePlane(P,sizeimg, siz, C, x_arrow_y);
+X_origin = ProjectToImagePlane(P,sizeimg, siz, C, [0 0 1]');
+hold on
+h = [h plot3(X_arrow_x(1,:), X_arrow_x(2,:), X_arrow_x(3,:), 'color',color)];
+h = [h plot3(X_arrow_y(1,:), X_arrow_y(2,:), X_arrow_y(3,:), 'color',color)];
+h = [h plot3(X_origin(1,:), X_origin(2,:), X_origin(3,:), 'o','color',color)];
+
+if false
+    text(X_arrow_x(1,1),X_arrow_x(2,1),X_arrow_x(3,1),'x','color',color)
+    text(X_arrow_y(1,1),X_arrow_y(2,1),X_arrow_y(3,1),'y','color',color)
+end
+hold off
+return
+
+function n = ProjectToImagePlane(P,sizeimg,siz,C,x)
+x(1:2,:) = bsxfun(@times, x(1:2,:), sizeimg);
+
+%Vectors pointing away from camera
+PP = [P;0 0 0 1];
+n = wnorm(PP\[x;ones(1,size(x,2))]);
+%n = wnorm(pinv(P)*x); %Using pinv does not work unfortunately
+n(1:3,:) = bsxfun(@minus,n(1:3,:),C(1:3,:));
+%Normalize to unit length
+n(1:3,:) = bsxfun(@rdivide, n(1:3,:), sqrt(sum(n(1:3,:).^2)));
+n(1:3,:) = n(1:3,:)*siz;
+
+%Add camera location to corner points
+n(1:3,:) = bsxfun(@plus,n(1:3,:),C(1:3)); 
+
+return
